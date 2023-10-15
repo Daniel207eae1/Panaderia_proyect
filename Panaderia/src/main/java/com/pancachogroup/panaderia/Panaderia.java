473a2ea409f5ce4714a0f6689689b0e367cb4f24
Daniel207eae1 <elbotcrack@gmail.com>
2023-10-15 08:42:33 -0500

    Configuraciones_iniciales
    pom.xml y configuracion inicial

---
+++ Panaderia/src/main/java/com/pancachogroup/panaderia/Panaderia.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.pancachogroup.panaderia;
+
+import Vistas.Inicio;
+
+
+public class Panaderia {
+
+    public static void main(String[] args) {
+        Inicio inicio_jframe = new Inicio();
+        inicio_jframe.setVisible(true);
+    }
+}
